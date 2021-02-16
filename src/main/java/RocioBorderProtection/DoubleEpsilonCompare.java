d6fa826830c6c115ccc63d980a9efe7d300b8bae
mhaghpanah <40441978+mhaghpanah@users.noreply.github.com>
2021-02-16 12:27:02 -0600

    Update DoubleEpsilonCompare.java

--- src/main/java/RocioBorderProtection/DoubleEpsilonCompare.java
+++ src/main/java/RocioBorderProtection/DoubleEpsilonCompare.java
@@ -5,12 +5,16 @@ public class DoubleEpsilonCompare {
   private static final double EPSILON = 1e-18;
 
   public static boolean equals(final double a, final double b) {
-    if (a == b) return true;
+    if (a == b) {
+      return true;
+    }
     return Math.abs(a - b) < EPSILON;
   }
 
   public static int compare(final double a, final double b) {
-    if (equals(a, b)) return 0;
+    if (equals(a, b)) {
+      return 0;
+    }
     return a < b ? -1 : +1;
   }
 
