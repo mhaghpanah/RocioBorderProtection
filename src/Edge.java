12ee28263e85b3347d6f5b6fa0ae60ec52d22713
mhaghpanah <40441978+mhaghpanah@users.noreply.github.com>
2021-02-16 12:18:02 -0600

    Create Edge.java

---
+++ src/Edge.java
@@ -0,0 +1,46 @@
+package RocioBorderProtection;
+
+public class Edge {
+
+  public enum EdgeType {
+      INTERIOR, EXTERIOR, BOUNDARY
+  }
+
+  private final Vertex u;
+  private final Vertex v;
+  private final double dist;
+  private final EdgeType edgeType;
+
+  public Edge(Vertex u, Vertex v, double dist, EdgeType edgeType) {
+    this.u = u;
+    this.v = v;
+    this.dist = dist;
+    this.edgeType = edgeType;
+  }
+
+  public Vertex getU() {
+    return u;
+  }
+
+  public EdgeType getEdgeType() {
+    return edgeType;
+  }
+
+  public Vertex getV() {
+    return v;
+  }
+
+  public double getDist() {
+    return dist;
+  }
+
+  @Override
+  public String toString() {
+    return "Edge{" +
+        "u=" + u +
+        ", v=" + v +
+        ", dist=" + dist +
+        ", edgeType=" + edgeType +
+        '}';
+  }
+}
