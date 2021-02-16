9fc4f09f0e3ebbaed6e39e8e55f6774143f2a4aa
mhaghpanah <40441978+mhaghpanah@users.noreply.github.com>
2021-02-16 12:33:17 -0600

    Update Pair.java

--- src/main/java/RocioBorderProtection/Pair.java
+++ src/main/java/RocioBorderProtection/Pair.java
@@ -10,15 +10,15 @@ public class Pair<K, V> {
     this.v = v;
   }
 
+  public static <K, V> Pair<K, V> getInstance(K k, V v) {
+    return new Pair<>(k, v);
+  }
+
   public K getKey() {
     return k;
   }
 
   public V getValue() {
     return v;
-  }
-
-  public static <K, V> Pair<K, V> getInstance(K k, V v) {
-    return new Pair<>(k, v);
   }
 }
