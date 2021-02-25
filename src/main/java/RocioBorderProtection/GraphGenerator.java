102a7e9ec0957f891b074e39ae4238ea5c380b17
mhaghpanah <40441978+mhaghpanah@users.noreply.github.com>
2021-02-25 13:25:29 -0600

    Update GraphGenerator.java

--- src/main/java/RocioBorderProtection/GraphGenerator.java
+++ src/main/java/RocioBorderProtection/GraphGenerator.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class GraphGenerator {
 
-  List<Point> pointList;
+  Points points;
   double capacity;
   double epsilon;
 
@@ -13,87 +13,112 @@ public class GraphGenerator {
 
   Graph graph;
 
-  public GraphGenerator(List<Point> pointList, double capacity, double epsilon) {
-    int n = pointList.size();
-    assert pointList.get(0).equals(pointList.get(n - 1));
-
-    this.pointList = pointList;
+  public GraphGenerator(Points points, double capacity, double epsilon) {
+    this.points = points;
     this.capacity = capacity;
     this.epsilon = epsilon;
 
-//    border = new ArrayList<>();
-    myPolygon = new MyPolygon(pointList);
+    myPolygon = new MyPolygon(points);
     graph = new Graph();
 
-//    generateBorder();
-//    assert isSimple();
     generateVertices();
     generateEdges();
   }
 
-//
-//  public boolean isSimple() {
-//    int n = border.size();
-//    for (int i = 0; i < n; i++) {
-//      if (border.get(i).isEmpty()) {
-//        return false;
-//      }
-//    }
-//
-//    for (int i = 0; i < n; i++) {
-//      for (int j = i + 1; j < n; j++) {
-//        Segment s0 = border.get(i);
-//        Segment s1 = border.get(j);
-//        if (s0.isIntersect(s1)) {
-//          return false;
-//        }
-//      }
-//    }
-//    return true;
-//  }
-
-  public static Graph GraphGenerator(List<Point> pointList, double unitDist, double eps) {
-    GraphGenerator gg = new GraphGenerator(pointList, unitDist, eps);
+  public static Graph generate(Points points, double capacity, double epsilon) {
+    GraphGenerator gg = new GraphGenerator(points, capacity, epsilon);
     return gg.graph;
   }
+
+//  public static Graph generate(Points points, int delta, double capacity, double epsilon) {
+//    Points points1 = Points.movePoints(points, delta);
+//    GraphGenerator gg = new GraphGenerator(points1, capacity, epsilon);
+//    return gg.graph;
+//  }
 
   private void generateVertices() {
     int n = myPolygon.size();
     int t = 0;
     for (int i = 0; i < n; i++) {
       List<Point> decompose = myPolygon.subdivision(i, epsilon);
-      System.out.printf("Subdivide %d into %d parts\n", i, decompose.size());
+//      System.out.printf("Subdivide %d into %d parts\n", i, decompose.size());
       assert decompose.size() > 0;
       for (Point point : decompose) {
-        Vertex vertex = new Vertex(t++, point, i);
+        boolean isOnConvexHull = myPolygon.isOnConvexHull(point);
+        Vertex vertex = new Vertex(t++, point, i, isOnConvexHull);
         graph.addVertex(vertex);
       }
-      System.out.printf("size: %d\n", graph.vertexSize());
+//      System.out.printf("size: %d\n", graph.vertexSize());
     }
 
-    Point point = pointList.get(n);
-    Vertex vertex = new Vertex(t++, point, n - 1);
+    Point point = points.get(n);
+    boolean isOnConvexHull = myPolygon.isOnConvexHull(point);
+    Vertex vertex = new Vertex(t, point, n - 1, isOnConvexHull);
     graph.addVertex(vertex);
-    System.out.printf("size: %d\n", graph.vertexSize());
-
-    System.out.println(pointList.get(0));
-    System.out.println(pointList.get(n));
-
-    System.out.println(graph.getVertex(0).getPoint());
-    System.out.println(graph.getVertex(graph.vertexSize() - 1).getPoint());
+//    System.out.printf("size: %d\n", graph.vertexSize());
 
     assert graph.getVertex(0).getPoint().equals(
         graph.getVertex(graph.vertexSize() - 1).getPoint());
   }
 
-  public boolean isLeftSide(int i, int j, Segment segment) {
-    for (int k = i + 1; k < j; k++) {
-      Vertex v = graph.getVertex(k);
-      if (segment.side(v.getPoint()) <= 0) {
-        return false;
-      }
+//  public boolean isLeftSide(int i, int j, Segment segment) {
+//    for (int k = i + 1; k < j; k++) {
+//      Vertex v = graph.getVertex(k);
+//      if (segment.side(v.getPoint()) < 0) {
+//        return false;
+//      }
+//    }
+//    return true;
+//  }
+
+  public double curve(Point a, Point b) {
+    return (b.getX() - a.getX()) * (a.getY() + b.getY());
+  }
+
+  public boolean isCCWSubChain(int i, int j) {
+    double sum = 0.0;
+
+    for (int k = i; k < j; k++) {
+      sum += curve(graph.getVertex(k).getPoint(), graph.getVertex(k + 1).getPoint());
     }
-    return true;
+    sum += curve(graph.getVertex(j).getPoint(), graph.getVertex(i).getPoint());
+
+    return DoubleEpsilonCompare.sign(sum) >= 0;
+  }
+
+
+  private Edge generateEdge(Vertex u, Vertex v) {
+    Segment segment = new Segment(u.getPoint(), v.getPoint());
+    double len = segment.getLength();
+
+    assert (u.getId() == 0 && v.getId() == graph.vertexSize() - 1)
+        || DoubleEpsilonCompare.compare(len, 0) > 0;
+
+    if (DoubleEpsilonCompare.compare(len, capacity) >= 0) {
+      return null;
+    }
+
+    if (u.getId() + 1 == v.getId()) {
+      return new Edge(u, v, len, EdgeType.BOUNDARY);
+    }
+
+    if (u.getSubdivisionId() == v.getSubdivisionId()) {
+      return null;
+    }
+
+    if (
+//        DoubleEpsilonCompare.compare(len, 0) > 0 &&
+//        DoubleEpsilonCompare.compare(len, capacity) <= 0 &&
+        myPolygon.isSegmentVisible(segment) &&
+//        myPolygon.isSegmentOutside(segment) &&
+            isCCWSubChain(u.getId(), v.getId())
+//            isLeftSide(u.getId(), v.getId(), segment)
+    ) {
+      return new Edge(u, v, len, EdgeType.EXTERIOR);
+    }
+
+    return null;
+
   }
 
   private void generateEdges() {
@@ -101,27 +126,17 @@ public class GraphGenerator {
     for (int i = 0; i < n; i++) {
       Vertex u = graph.getVertex(i);
       for (int j = i + 1; j < n; j++) {
+        if (i == 0 && j == n - 1) {
+          continue;
+        }
         Vertex v = graph.getVertex(j);
-        Segment segment = new Segment(u.getPoint(), v.getPoint());
-        double len = segment.getLength();
-        if (u.getSubdivisionId() == v.getSubdivisionId() ||
-            (u.getSubdivisionId() + 1 == v.getSubdivisionId() && i + 1 == j)) {
-          if (j == i + 1) {
-            Edge edge = new Edge(u, v, len, EdgeType.BOUNDARY);
-            graph.addEdge(edge);
-          }
-        } else if (DoubleEpsilonCompare.compare(len, 0) > 0 && 
-            DoubleEpsilonCompare.compare(len, capacity) < 0 && 
-            myPolygon.isSegmentVisible(segment) && 
-            myPolygon.isSegmentOutside(segment) && 
-            isLeftSide(i, j, segment)) {
-          Edge edge = new Edge(u, v, len, EdgeType.EXTERIOR);
+        Edge edge = generateEdge(u, v);
+        if (edge != null) {
           graph.addEdge(edge);
         }
 
       }
     }
-    System.out.printf("sz: %d\n", graph.edgeSize());
   }
 
 }
