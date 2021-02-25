b3499f46a8e1993b0ad5fe075bc2951ad6c9dcca
mhaghpanah <40441978+mhaghpanah@users.noreply.github.com>
2021-02-25 13:32:35 -0600

    Update MapDrawer.java

--- src/main/java/RocioBorderProtection/MapDrawer.java
+++ src/main/java/RocioBorderProtection/MapDrawer.java
@@ -1,14 +1,14 @@
 package RocioBorderProtection;
 
-import static OrderTypeGraph.IpeDraw.drawIpeMark;
-import static OrderTypeGraph.IpeDraw.drawIpePath;
-import static OrderTypeGraph.IpeDraw.getIpeConf;
-import static OrderTypeGraph.IpeDraw.getIpeEnd;
-import static OrderTypeGraph.IpeDraw.getIpePreamble;
-import static OrderTypeGraph.IpeDraw.writeIpeText;
+import static RocioBorderProtection.IpeDraw.drawIpeMark;
+import static RocioBorderProtection.IpeDraw.drawIpePath;
+import static RocioBorderProtection.IpeDraw.getIpeConf;
+import static RocioBorderProtection.IpeDraw.getIpeEnd;
+import static RocioBorderProtection.IpeDraw.getIpePreamble;
+import static RocioBorderProtection.IpeDraw.writeIpeText;
 
-import OrderTypeGraph.MyFileWriter;
 import RocioBorderProtection.Edge.EdgeType;
+import java.util.Collection;
 import java.util.List;
 
 public class MapDrawer {
@@ -24,56 +24,100 @@ public class MapDrawer {
 
   StringBuilder output;
 
-  List<Point> pointList;
+  Points points;
   List<List<Edge>> path;
+  Graph graph;
   boolean addText;
 
 
-  public MapDrawer(List<Point> pointList, boolean addText) {
-    this.pointList = pointList;
+  public MapDrawer(Points points, boolean addText) {
+    this.points = points;
     this.addText = addText;
 
-    scaleFactor = computeScaleFactor();
+    scaleFactor = computeScaleFactor(points);
     output = new StringBuilder();
 
     output.append(getIpePreamble());
     output.append(getIpeConf());
-    drawPoints();
-    drawEdges();
+    drawPoints(points);
+    drawPolygonEdges(points);
+
+//    String color = "red";
+//    String pen = "normal";
+//    String dash = "normal";
+//
+//    drawEdge(points.get(591), points.get(592), color, pen, dash);
+//    drawEdge(points.get(592), points.get(593), color, pen, dash);
+
 //    output.append(getIpeEnd());
   }
 
-  public MapDrawer(List<Point> pointList, List<List<Edge>> path, boolean addText) {
-    this(pointList, addText);
+  public MapDrawer(Points points, List<List<Edge>> path, boolean addText) {
+    this(points, addText);
     this.path = path;
-    drawPath();
+    drawPath(path);
   }
 
-  public static String draw(List<Point> pointList, boolean addText) {
-    MapDrawer mapDrawer = new MapDrawer(pointList, addText);
+  public MapDrawer(Graph graph, List<List<Edge>> path, boolean addText) {
+    this.graph = graph;
+    this.path = path;
+    this.addText = addText;
+    this.points = graph.getPoints();
+
+    scaleFactor = computeScaleFactor(points);
+    output = new StringBuilder();
+
+    output.append(getIpePreamble());
+    output.append(getIpeConf());
+    drawVertexes(graph.getVertexList());
+    drawPolygonEdges(points);
+
+//    List<List<Edge>> ll = new ArrayList<>();
+//    ll.add(graph.getAdjacencyMap().get(graph.getVertex(1369)));
+//    drawEdges(ll);
+    drawEdges(graph.getAdjacencyMap().values());
+    drawPath(path);
+
+    MyPolygon myPolygon = new MyPolygon(points);
+    drawConvexHull(myPolygon.generateConvexHull());
+
+  }
+
+  public static String draw(Points points, boolean addText) {
+    MapDrawer mapDrawer = new MapDrawer(points, addText);
     return mapDrawer.output();
   }
 
-  public static void drawAndWrite(List<Point> pointList, String suffixPathOutput, boolean addText) {
-    MyFileWriter.write(suffixPathOutput + ".ipe", MapDrawer.draw(pointList, addText));
+  public static void drawAndWrite(Points points, String suffixPathOutput, boolean addText) {
+    MyFileWriter.write(suffixPathOutput + ".ipe", MapDrawer.draw(points, addText));
   }
 
-  public static String draw(List<Point> pointList, List<List<Edge>> path, boolean addText) {
-    MapDrawer mapDrawer = new MapDrawer(pointList, path, addText);
+  public static String draw(Points points, List<List<Edge>> path, boolean addText) {
+    MapDrawer mapDrawer = new MapDrawer(points, path, addText);
     return mapDrawer.output();
   }
 
-  public static void drawAndWrite(List<Point> pointList, List<List<Edge>> path,
+  public static void drawAndWrite(Points points, List<List<Edge>> path,
       String suffixPathOutput, boolean addText) {
-    MyFileWriter.write(suffixPathOutput + ".ipe", MapDrawer.draw(pointList, path, addText));
+    MyFileWriter.write(suffixPathOutput + ".ipe", MapDrawer.draw(points, path, addText));
   }
 
-  private double computeScaleFactor() {
+  public static String draw(Graph graph, List<List<Edge>> path, boolean addText) {
+    MapDrawer mapDrawer = new MapDrawer(graph, path, addText);
+    return mapDrawer.output();
+  }
+
+  public static void drawAndWrite(Graph graph, List<List<Edge>> path, String suffixPathOutput,
+      boolean addText) {
+    MyFileWriter.write(suffixPathOutput + ".ipe", MapDrawer.draw(graph, path, addText));
+  }
+
+  private double computeScaleFactor(Points points) {
     minX = Long.MAX_VALUE;
     minY = Long.MAX_VALUE;
     maxX = Long.MIN_VALUE;
     maxY = Long.MIN_VALUE;
-    for (Point point : pointList) {
+    for (Point point : points.getPointList()) {
       minX = Math.min(minX, point.getX());
       minY = Math.min(minY, point.getY());
 
@@ -84,57 +128,146 @@ public class MapDrawer {
     double deltaX = maxX - minX;
     double deltaY = maxY - minY;
     return drawSize / Math.max(deltaX, deltaY);
-//    return Math.min(1.0, drawSize / (maxCoordinate - minCoordinate));
-//    return drawSize / (maxCoordinate - minCoordinate);
-
   }
 
-  private int scaleX(double t) {
-    return (int) (((t - minX) * scaleFactor) + o);
-//    return (int) (t / 3 + o);
+  private double scaleX(double t) {
+    return (((t - minX) * scaleFactor) + o);
   }
 
-  private int scaleY(double t) {
-    return (int) (((t - minY) * scaleFactor) + o);
-  }
-
-  private void drawEdge(Point u, Point v, String color, String pen, String dash) {
-    int ux = scaleX(u.getX());
-    int uy = scaleY(u.getY());
-
-    int vx = scaleX(v.getX());
-    int vy = scaleY(v.getY());
-
-    output.append(drawIpePath(new int[]{ux, vx}, new int[]{uy, vy}, color, pen, dash));
-  }
-
-  private void drawPoint(Point point, String shape, String color, String size) {
-    int x = scaleX(point.getX());
-    int y = scaleY(point.getY());
-    output.append(drawIpeMark(x, y, shape, color, size));
+  private double scaleY(double t) {
+    return (((t - minY) * scaleFactor) + o);
   }
 
   private void drawText(String string, Point point, String color, String size) {
-    int eps = 3;
-    int x = scaleX(point.getX());
-    int y = scaleY(point.getY());
+    double eps = 3;
+    double x = scaleX(point.getX());
+    double y = scaleY(point.getY());
     output.append(writeIpeText(string, x - eps, y + eps, color, size));
   }
 
-  private void drawPath() {
+  private void drawPoint(Point point, String shape, String color, String size) {
+    double x = scaleX(point.getX());
+    double y = scaleY(point.getY());
+    output.append(drawIpeMark(x, y, shape, color, size));
+  }
+
+  private void drawPoints(Points points, String shape, String color, String size) {
+    int index = 0;
+    for (Point point : points.getPointList()) {
+      drawPoint(point, shape, color, size);
+      if (addText) {
+        if (index % 5 == 0) {
+          drawText(Integer.toString(index), point, color, size);
+        }
+        index++;
+      }
+    }
+  }
+
+  private void drawPoints(Points points) {
+    String shape = "disk";
+    String color = "black";
+    String size = "normal";
+
+    drawPoints(points, shape, color, size);
+  }
+
+
+  private void drawVertexes(List<Vertex> vertexList) {
+    String shape = "disk";
+    String colorBlack = "black";
+    String colorBlue = "blue";
+    String size = "normal";
+
+    int index = 0;
+    for (Vertex vertex : vertexList) {
+      Point point = vertex.getPoint();
+
+      String color = vertex.isOnConvexHull() ? colorBlue : colorBlack;
+//      if (vertex.getId() == 1369 || (1380 <= vertex.getId() && vertex.getId() <= 1384)) color = "red";
+
+      drawPoint(point, shape, color, size);
+      if (addText) {
+        if (index % 5 == 0) {
+          drawText(Integer.toString(index), point, color, size);
+        }
+        index++;
+      }
+    }
+  }
+
+  private void drawEdge(Point u, Point v, String color, String pen, String dash) {
+    double ux = scaleX(u.getX());
+    double uy = scaleY(u.getY());
+
+    double vx = scaleX(v.getX());
+    double vy = scaleY(v.getY());
+
+    output.append(drawIpePath(new double[]{ux, vx}, new double[]{uy, vy}, color, pen, dash));
+  }
+
+  private void drawPolygonEdges(Points points, String color, String pen, String dash) {
+    int n = points.size();
+    for (int i = 0; i < n; i++) {
+      Point u = points.get(i);
+      Point v = points.get(i + 1);
+//      if (i % 2 == 0) {
+//        color = "green";
+//      } else {
+//        color = "red";
+//      }
+      drawEdge(u, v, color, pen, dash);
+    }
+  }
+
+  private void drawPolygonEdges(Points points) {
+    String color = "black";
+    String pen = "normal";
+    String dash = "normal";
+    drawPolygonEdges(points, color, pen, dash);
+  }
+
+
+  private void drawEdges(Collection<List<Edge>> edges, String color, String pen, String dash) {
+    for (List<Edge> edgeList : edges) {
+      for (Edge edge : edgeList) {
+        Point u = edge.getU().getPoint();
+        Point v = edge.getV().getPoint();
+
+        drawEdge(u, v, color, pen, dash);
+      }
+    }
+  }
+
+  private void drawEdges(Collection<List<Edge>> edges) {
+    String color = "black";
+    String pen = "normal";
+    String dash = "normal";
+    drawEdges(edges, color, pen, dash);
+  }
+
+  private void drawPath(List<List<Edge>> path) {
     String shape = "disk";
     String color = "red";
     String size = "normal";
 
+    int index = 0;
     for (List<Edge> edgeList : path) {
       Vertex u = edgeList.get(0).getU();
       Point point = u.getPoint();
       drawPoint(point, shape, color, size);
+
+      if (addText) {
+        if (index % 10 == 0) {
+          drawText(Integer.toString(index), point, color, size);
+        }
+        index++;
+      }
     }
 
     String pen = "normal";
     String dash_normal = "normal";
-    String dash_dashed = "dashed";
+//    String dash_dashed = "dashed";
     String dash_dotted = "dotted";
 
     for (List<Edge> edgeList : path) {
@@ -142,52 +275,31 @@ public class MapDrawer {
         Point u = edge.getU().getPoint();
         Point v = edge.getV().getPoint();
         if (edge.getEdgeType().equals(EdgeType.BOUNDARY)) {
-//         String dash = dash_normal;
-//         drawEdge(u, v, color, pen, dash);
+          drawEdge(u, v, color, pen, dash_dotted);
         } else {
-          String dash = dash_normal;
-          drawEdge(u, v, color, pen, dash);
+          drawEdge(u, v, color, pen, dash_normal);
         }
       }
     }
 
   }
 
-  private void drawEdges() {
 
-    String color = "black";
+  private void drawConvexHull(Points points) {
+//    String shape = "disk";
+//    String colorPoints = "seagreen";
+//    String size = "normal";
+//    drawPoints(points, shape, colorPoints, size);
+
+    String colorEdges = "seagreen";
     String pen = "normal";
-    String dash = "normal";
+    String dash = "dashed";
+    drawPolygonEdges(points, colorEdges, pen, dash);
 
-    for (int i = 0; i < pointList.size() - 1; i++) {
-      Point u = pointList.get(i);
-      Point v = pointList.get(i + 1);
-
-      drawEdge(u, v, color, pen, dash);
-    }
-    System.out.printf("here pointlist %s\n", pointList.get(0));
-    System.out.printf("here pointlist %s\n", pointList.get(pointList.size() - 1));
-
-  }
-
-  private void drawPoints() {
-    String shape = "disk";
-    String color = "black";
-    String size = "normal";
-
-    int index = 0;
-    for (Point point : pointList) {
-      drawPoint(point, shape, color, size);
-      if (addText) {
-        drawText(Integer.toString(index++), point, color, size);
-      }
-    }
   }
 
   public String output() {
-    StringBuilder ans = new StringBuilder(output);
-    ans.append(getIpeEnd());
-    return ans.toString();
+    return output + getIpeEnd();
   }
 
 }
