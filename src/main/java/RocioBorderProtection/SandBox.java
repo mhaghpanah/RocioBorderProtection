2927373d46c339c344bdc5f18c7c4ab62e1999a5
mhaghpanah <40441978+mhaghpanah@users.noreply.github.com>
2021-02-25 18:27:03 -0600

    Update SandBox.java

--- src/main/java/RocioBorderProtection/SandBox.java
+++ src/main/java/RocioBorderProtection/SandBox.java
@@ -28,7 +28,39 @@ public class SandBox {
 
   }
 
-  public static void exp(Points points, double epsilon, String suffixPath) {
+  public static boolean isOptimal(Points points, double capacity, double epsilon) {
+    Graph graph0 = GraphGenerator.generate(points, capacity, epsilon);
+    List<List<Edge>> path0 = Hop.run(graph0, capacity);
+
+    Graph graph1 = GraphGenerator.generate(points, capacity + 2 * epsilon, epsilon);
+    List<List<Edge>> path1 = Hop.run(graph1, capacity);
+
+    assert path1.size() >= path0.size();
+
+    System.out.printf("Capacity: %f Epsilon: %f Path0 sz: %d Path1 sz: %d\n", capacity, epsilon, path0.size(), path1.size());
+
+    return path1.size() == path0.size();
+  }
+
+  public static void solve(Points points, double capacity, String suffixPath) {
+    double epsilon = 0.001;
+    while (!isOptimal(points, capacity, epsilon)) {
+      System.out.printf("Capacity: %f Epsilon: %f\n", capacity, epsilon);
+      epsilon = epsilon / 2.0;
+    }
+    System.out.printf("Capacity: %f Epsilon: %f\n", capacity, epsilon);
+
+    Graph graph = GraphGenerator.generate(points, capacity, epsilon);
+    runHop(graph, capacity, suffixPath);
+  }
+
+  public static void exp1(Points points, String suffixPath) {
+    for (double capacity = 0.02; capacity <= 0.04; capacity += 0.001) {
+      solve(points, capacity, suffixPath);
+    }
+  }
+
+  public static void exp0(Points points, double epsilon, String suffixPath) {
     double c = 5 * epsilon;
     for (int i = 0; i < 12; i++) {
       double capacity = c * Math.pow(2, i);
@@ -41,15 +73,17 @@ public class SandBox {
     String island = "salamis";
     String suffixFilePath = String.format("%s.txt", island);
 
+    Points points = PrecisionReadPoints.inexactRead(suffixFilePath);
+
+
     double epsilon = 0.001;
     double capacity = 10000 * epsilon;
 
-    Points points = PrecisionReadPoints.inexactRead(suffixFilePath);
 
     Graph graph = generateAndDraw(points, capacity, epsilon, island);
     runHop(graph, capacity, island);
-
-    exp(points, epsilon, island);
+//
+//    exp0(points, epsilon, island);
 
 //    MyPolygon myPolygon = new MyPolygon(points);
 //    MyPolygon convexHull = new MyPolygon(myPolygon.generateConvexHull());
@@ -61,6 +95,10 @@ public class SandBox {
 //      Graph graph = generateAndDraw(points, capacity, epsilon, island);
 //      runHop(graph, capacity, island);
 //    }
+
+    exp1(points, island);
+
+
 
   }
 
